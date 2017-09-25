707aef485cb8a58c9b0afe3bfa5783451b6d43ac
David J Eddy <me@davidjeddy.com>
2017-09-25 17:38:54 -0400

    Basic file IO

---
+++ youtube_python_programming/file_io.py
@@ -0,0 +1,17 @@
+import random
+import sys
+import os
+
+test_file = open("test.txt", "wb")
+print(test_file.mode)
+print(test_file.name)
+test_file.write(bytes("Write me to the file", 'UTF-8'))
+test_file.close()
+
+test_file = open("test.txt", "r+")
+
+text_in_file = test_file.read()
+
+print(text_in_file)
+
+os.remove('test.txt')
