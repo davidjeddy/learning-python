b08f6fd2648ab6629b9e3b5a6fe88cd995da7ae7
David J Eddy <me@davidjeddy.com>
2017-09-25 16:14:57 -0400

    Finished  examples.

--- youtube_python_programming/list.py
+++ youtube_python_programming/list.py
@@ -6,3 +6,39 @@ grocery_list = ['Juice', 'Tomatoes', 'Potatoes']
 
 print('First item is: ', grocery_list[0])
 
+grocery_list[0] = "Green Juice"
+
+print('First item is: ', grocery_list[0])
+
+# up to but no include last id'd item
+print('First item is: ', grocery_list[1:3])
+
+other_events = ['Wash Cart', 'Pick up', 'Get check']
+
+todo = [other_events, grocery_list]
+
+print(todo)
+
+print(todo[1][1])
+
+grocery_list.append("Onion")
+print(grocery_list)
+
+grocery_list.insert(1, "Pickle")
+print(grocery_list)
+
+grocery_list.remove("Pickle")
+
+grocery_list.sort()
+grocery_list.reverse()
+
+del grocery_list[4]
+print(grocery_list)
+
+# combine lists together
+todo2 = grocery_list + todo;
+print(len(todo2))
+
+print(max(todo2))
+
+print(min(todo2))
